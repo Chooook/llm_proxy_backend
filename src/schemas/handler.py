43f89a2a210c3b77b9c576446407849f3bfebef7
Chooook <egr311@ya.ru>
2025-07-14 01:59:12 +0400

    chore: update handler schema

--- src/schemas/handler.py
+++ src/schemas/handler.py
@@ -4,9 +4,15 @@ from pydantic import BaseModel, computed_field
 class HandlerConfig(BaseModel):
     name: str
     task_type: str
-    import_path: str
     version: str
     description: str = ''
+
+    import_path: str  # TODO rename to interface_function_import_path
+    interface_endpoint_url: str = ''
+
+    handler_repo: str = ''
+    db_loader_script_path: str = ''
+    service_launcher_script_path: str = ''
 
     @computed_field(return_type=str)
     @property
