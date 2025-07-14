f7426fb0f6fb3937934e92d13862712c35e270c0
Chooook <egr311@ya.ru>
2025-07-15 03:19:44 +0400

    feat: add single_connection_gp_utils.py, rework kerberos ticket renewal

--- src/utils/single_connection_gp_utils.py
+++ src/utils/single_connection_gp_utils.py
@@ -1,73 +1,61 @@
 import subprocess
-from contextlib import asynccontextmanager
 from datetime import datetime, timedelta
-from urllib.parse import quote_plus
 
 import asyncio_atexit
 import asyncpg
-from asyncpg import Pool
 from loguru import logger
 
 from settings import settings
 
 USERNAME = settings.GP_USERNAME
-PASSWORD = quote_plus(settings.GP_PASSWORD)
 HOST = settings.GP_HOST
 PORT = settings.GP_PORT
 DATABASE = settings.GP_DATABASE
 
-MIN_CONNECTIONS = settings.GP_MIN_CONNECTIONS
-MAX_CONNECTIONS = settings.GP_MAX_CONNECTIONS
-MAX_INACTIVE_LIFETIME = settings.GP_MAX_INACTIVE_LIFETIME
-
-pool: Pool | None = None
+connection: asyncpg.Connection | None = None
 
 
 async def run_query(query, params=()):
     if not check_kerberos_ticket():
         renew_kerberos_ticket()
     try:
-        async with get_pg_conn() as conn:
-            return await conn.fetch(query, *params)
+        conn: asyncpg.Connection = await get_pg_conn()
+        return await conn.fetch(query, *params)
     except asyncpg.PostgresError as e:
         logger.error(f'⚠️ GP query failed: {e}')
         raise
 
 
-@asynccontextmanager
 async def get_pg_conn():
-    global pool
-    if pool is None:
+    global connection
+    if connection is None:
         await __init_db()
-    async with pool.acquire() as conn:
-        yield conn
+    return connection
 
 
 async def __init_db():
-    global pool
-    if pool is not None:
+    global connection
+    if connection is not None:
         return
     if not check_kerberos_ticket():
         renew_kerberos_ticket()
 
-    pool = await asyncpg.create_pool(
-        dsn=f'postgresql://{USERNAME}@{HOST}:{PORT}/{DATABASE}',
-        min_size=MIN_CONNECTIONS,
-        max_size=MAX_CONNECTIONS,
-        max_inactive_connection_lifetime=MAX_INACTIVE_LIFETIME)
+    connection = await asyncpg.connect(
+        host=HOST,
+        port=PORT,
+        database=DATABASE,
+        user=USERNAME)
 
-    logger.info(
-        'ℹ️ Connection pool GP created '
-        f'(size: {MIN_CONNECTIONS}-{MAX_CONNECTIONS})')
+    logger.info(f'ℹ️ Connection for GP created')
     asyncio_atexit.register(__close_db)
 
 
 async def __close_db():
-    global pool
-    if pool:
-        await pool.close()
-        logger.info('ℹ️ Connection pool closed')
-        pool = None
+    global connection
+    if connection:
+        await connection.close()
+        logger.info('ℹ️ GP connection closed')
+        connection = None
 
 
 def check_kerberos_ticket() -> bool:
